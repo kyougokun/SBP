54e31e4dba44644b94f1e3d12e7826bf247792b2
IkisugiGroup <iksg0810@gmail.com>
2019-03-27 07:13:27 +0900

    register event

--- src/igdt/Main.php
+++ src/igdt/Main.php
@@ -5,5 +5,7 @@ namespace igdt;
 use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase{
-	
+	public function onEnable(){
+		$this->getServer()->getPluginManager()->registerEvents(new Event(), $this);
+	}
 }
